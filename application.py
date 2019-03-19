f348ee37cf39df65f0e6d1ed51ebcdb9a9583e30
Jishu Dohare <sfsda@adsfadf.com>
2019-03-20 02:38:57 +0530

    Removed a error, if the user opens the load file dialog and doesn't select any file, then the application works perfectly, and wrote the addData function for adding row and column but it does not work fine

--- application.py
+++ application.py
@@ -75,12 +75,13 @@ class App(QWidget):
 
     def loadCsv(self):
         self.filename = (QFileDialog.getOpenFileName(self, 'Open CSV', os.getenv('HOME'), 'CSV(*.csv)'))[0]
-        fileinput = open(self.filename, 'r')
-        self.data = list(csv.reader(fileinput))
-        self.row_size = len(self.data)
-        self.column_size = max([len(self.data[0]), len(self.data[1]), len(self.data[2])])
-        self.fileOPened = True
-        self.createTable()
+        if self.filename!='':
+            self.fileOPened = True
+            fileinput = open(self.filename, 'r')
+            self.data = list(csv.reader(fileinput))
+            self.row_size = len(self.data)
+            self.column_size = max([len(self.data[0]), len(self.data[1]), len(self.data[2])])
+            self.createTable()
 
     def createTable(self):
         self.twig = QTableWidget()
@@ -143,10 +144,15 @@ class App(QWidget):
         else:
             print(flag)
             if flag=='Col':
-                pass
+                for i in range(self.row_size):
+                    self.twig.setItem(i, self.column_size, QTableWidgetItem(""))
+                self.column_size += 1
+            elif flag=='Row':
+                for i in range(self.column_size):
+                    self.twig.setItem(self.row_size, i, QTableWidgetItem(""))
+                self.row_size += 1
             else:
-                flag=='Row':
-            pass
+                QMessageBox.about(self, "Error", "Something is Wrong")
 
 app = QApplication(sys.argv)
 ex = App()
