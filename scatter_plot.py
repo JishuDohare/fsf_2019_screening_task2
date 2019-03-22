0e89d3363647382dbfff35438a5f4eedefcd06c7
Jishu Dohare <sfsda@adsfadf.com>
2019-03-22 20:02:23 +0530

    added ploting.py which is a basic example of using matplotlib in pyqt5, and added scatter_plot.py file to learn scatter plot

---
+++ scatter_plot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create data
+N = 500
+# x = np.random.rand(N)
+# y = np.random.rand(N)
+x = [1, 2, 3]
+y = [4, 1, 6]
+
+# Plot
+plt.scatter(x, y)
+plt.title('Scatter plot pythonspot.com')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
